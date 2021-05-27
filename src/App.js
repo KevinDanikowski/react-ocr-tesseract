8f9480c21623fddeb947c1a4819c067a6a0c0a4d
Kevin Danikowski <kdanikowski@outlook.com>
2021-05-26 21:04:03 -0500

    added ocr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
+import React, {
+  useEffect,
+  useState
+} from 'react';
 import './App.css';
+import Dropzone from 'react-dropzone-uploader';
+import {
+  createWorker
+} from "tesseract.js";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  const [text, setText] = useState(null);
+
+  const [imageUrl] = useState(null);
+
+
+  useEffect(() => {
+    if (imageUrl != null) {
+      ExtractTextFromImage();
+    }
+  });
+
+  const tesseractWorker = createWorker({
+    logger: (m) => console.log(m),
+    workerPath: '/'
+  });
+
+  const ExtractTextFromImage = async (imageUrl) => {
+    await tesseractWorker.load();
+    await tesseractWorker.loadLanguage("eng");
+    await tesseractWorker.initialize("eng");
+    const {
+      data: {
+        text
+      },
+
+    } = await tesseractWorker.recognize(imageUrl);
+    setText(text);
+    await tesseractWorker.terminate();
+  };
+
+
+  const getUploadParams = () => {
+    return {
+      url: 'https://httpbin.org/post'
+    }
+  }
+
+  const handleChangeStatus = ({
+    meta
+  }, status) => {
+    if (status === 'headers_received') {
+      alert("Uploaded");
+      setText("Reconizing...");
+      ExtractTextFromImage(meta.previewUrl);
+    } else if (status === 'aborted') {
+      alert("Something went wrong")
+    }
+  }
+
+
+  return (<React.Fragment>
+
+    <nav className="navbar navbar-light bg-light justify-content-center mt-3" >
+      <a className="navbar-brand" href="/" > React OCR </a><br />
+      <p> Optical Character Recognition with React and Tesseract.js </p>
+    </nav >
+
+
+    <Dropzone getUploadParams={
+      getUploadParams
+    }
+      onChangeStatus={
+        handleChangeStatus
+      }
+      maxFiles={
+        1
+      }
+      multiple={
+        false
+      }
+      canCancel={
+        false
+      }
+      accept="image/jpeg, image/png, image/jpg"
+      inputContent={
+        (files, extra) => (extra.reject ? 'Only PNG and JPG Image files are allowed' : 'Drop  image here')
+      }
+      styles={
+        {
+          dropzoneActive: {
+            borderColor: 'green'
+          },
+          dropzoneReject: { borderColor: 'red', backgroundColor: '#DAA' },
+          inputLabel: (files, extra) => (extra.reject ? { color: 'red' } : {}),
+        }
+      }
+    />  <div className="container text-center pt-5" > {
+      text
+    } </div>
+
+  </React.Fragment>
+  )
+};
 
 export default App;
